eae3ff5140c12a9bba1d1cc195d11a915b98d354
oksunwoo <oksunwo@naver.com>
2022-07-19 17:46:30 +0900

    initial commit

---
+++ Lecture1/Lecture1/Lecture1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lecture1App.swift
+//  Lecture1
+//
+//  Created by Sunwoo on 2022/07/19.
+//
+
+import SwiftUI
+
+@main
+struct Lecture1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
