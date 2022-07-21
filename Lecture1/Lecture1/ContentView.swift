adf7ee3fc778816f8489cd2593f0e453bd1966fa
oksunwoo <oksunwo@naver.com>
2022-07-21 23:04:29 +0900

    Assignment1: Memorize part1

--- Lecture1/Lecture1/ContentView.swift
+++ Lecture1/Lecture1/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = ["🚂","🚘", "🛵", "🛸", "🛴", "🚍", "🚛", "🚞", "📱", "⌚️", "💻", "🖥", "⚽️", "🏀", "🏈", "🥎", "⚾️", "🏓", "🤿", "🥊", "🎗", "🚁", "🚦"]
-    @State var emogiCount = 4
+    var vehicleEmojis = ["🚂","🚘", "🛵", "🛸", "🛴", "🚍", "🚛", "🚞", "🚁", "🚔", "🛺", "🚐", "🚒", "🚑" ,"🚜"]
+    var sportEmojis = ["⚽️", "🏀", "🏈", "🥎", "⚾️", "🏓", "🤿", "🥊", "🏹", "🪀", "⛳️", "🎱", "🪃", "🏸", "🏒"]
+    var flagEmojis = ["🇬🇭", "🇬🇦", "🇬🇾", "🇬🇲", "🇬🇬", "🇬🇺", "🇬🇹", "🇰🇷", "🇳🇪", "🇱🇷", "🇺🇸", "🇧🇩" , "🇲🇾", "🇸🇧", "🏴󠁧󠁢󠁥󠁮󠁧󠁿"]
+    @State var emojiCount = 4
     
     var body: some View {
         VStack{
+            Text("Memorize")
+                .font(.largeTitle)
             ScrollView{
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]) {
-                    ForEach(emojis[0..<emogiCount], id: \.self) { emoji in
+                    ForEach(vehicleEmojis[0..<emojiCount], id: \.self) { emoji in
                         CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
                     }
                 }
@@ -35,8 +39,8 @@ struct ContentView: View {
     
     var remove : some View {
         Button {
-            if emogiCount > 1 {
-                emogiCount -= 1
+            if emojiCount > 1 {
+                emojiCount -= 1
             }
         } label: {
             Image(systemName: "minus.circle")
@@ -45,8 +49,8 @@ struct ContentView: View {
     
     var add : some View {
         Button {
-            if emogiCount < emojis.count {
-                emogiCount += 1
+            if emojiCount < vehicleEmojis.count {
+                emojiCount += 1
             }
         } label: {
             Image(systemName: "plus.circle")
