8fae4daf817797875439d3412f80ddfd7ea0ae6e
oksunwoo <oksunwo@naver.com>
2022-07-23 23:29:44 +0900

    feat: View와 ViewModel 생성

--- Lecture1/Lecture1/EmojiMemoryGame.swift
+++ Lecture1/Lecture1/EmojiMemoryGame.swift
@@ -8,5 +8,17 @@
 import SwiftUI
 
 class EmojiMemoryGame  {
+    static let emojis = ["🚂","🚘", "🛵", "🛸", "🛴", "🚍", "🚛", "🚞", "🚁", "🚔", "🛺", "🚐", "🚒", "🚑" ,"🚜"," 🐸", "🐛", "🦋", "🚖", "🚝", "🚡"]
     
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            EmojiMemoryGame.emojis[pairIndex]
+        }
+    }
+    
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
 }
