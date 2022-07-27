3612163e67893105e34421166986c88b1b68c615
oksunwoo <oksunwo@naver.com>
2022-07-27 20:23:27 +0900

    refactor: 카드 안의 이미지가 카드의 사이즈에 따라 자동으로 변하도록 변경

--- Lecture1/Lecture1/EmojiMemoryGameView.swift
+++ Lecture1/Lecture1/EmojiMemoryGameView.swift
@@ -18,7 +18,7 @@ struct EmojiMemoryGameView: View {
                     Spacer()
                     Text("Score: \(game.gamePoint)")
                 }.foregroundColor(.white)
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
                     ForEach(game.cards) { card in
                         CardView(card: card)
                             .aspectRatio(2/3, contentMode: .fit)
@@ -45,22 +45,35 @@ struct CardView: View {
     let card: EmojiMemoryGame.Card
     
     var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 20)
-            if card.isFaceUp {
-                shape
-                    .fill()
-                    .foregroundColor(.white)
-                shape
-                    .strokeBorder(lineWidth: 3)
-                Text(card.content)
-                    .font(.largeTitle)
-            } else if card.isMatched {
-                shape.opacity(0)
-            } else {
-                shape.fill()
+        GeometryReader { geometry in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+                if card.isFaceUp {
+                    shape
+                        .fill()
+                        .foregroundColor(.white)
+                    shape
+                        .strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    Text(card.content)
+                        .font(font(in: geometry.size))
+                } else if card.isMatched {
+                    shape.opacity(0)
+                } else {
+                    shape.fill()
+                }
             }
         }
+    }
+    
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    }
+    
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 20
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.7
     }
 }
 
